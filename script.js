bf3f634ace7737a3adffdd36e50ee1af090c8356
Leonard <leonardpocrncic@gmail.com>
2025-05-21 17:32:28 +0200

    add project images

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 `use strict`;
 
 let showAllButton = document.querySelector(".show-all-button");
-let showAllText = document.querySelector(".show-all-text");
-let showAllIcon = document.querySelector(".show-all-icon");
 let gridContainer = document.getElementById("grid-container");
 
 showAllButton.addEventListener("click", function () {
-  showAllText.textContent === `Show all`
-    ? (showAllText.textContent = `Show less`)
-    : (showAllText.textContent = `Show all`);
+  showAllButton.textContent === `Show all`
+    ? (showAllButton.textContent = `Show less`)
+    : (showAllButton.textContent = `Show all`);
   gridContainer.classList.toggle(`shown`);
-  showAllIcon.classList.toggle(`ph-caret-down`);
-  showAllIcon.classList.toggle(`flip`);
 });
